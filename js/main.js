9e0f8ea892a49e9458ce38ac466d99d746824d23
gkarumba <gachegua@gmail.com>
2019-05-13 08:00:45 +0300

    Add the resetGame function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-class Player{
-    constructor(name,total,show){
-        this.name = name;
-        this.total = total;
-        this.show = show;
-    }
-}
+// class Player{
+//     constructor(name,total,show){
+//         this.name = name;
+//         this.total = total;
+//         this.show = show;
+//     }
+// var player1 = new Player() ;
+var totalP1 = 0;
+var totalP2 = 0;
 var arrays = [1,2,3,4,5,6];
+var showP1 = [];
+var showP2 = [];
+var arrays = [1,2,3,4,5,6];
+$(document).ready(function(){
+    arrays.forEach(function(array){
+        $("#"+array).hide();
+    });
+    var resetStuff = function(){
+        arrays.forEach(function(array){
+            $("#"+array).hide();
+            totalP1 = 0;
+            totalP2 = 0;
+            $("#score-p1").text(totalP1);
+            $("#score-p2").text(totalP2);
+            $(".role-dice-player2").show();
+            $(".role-dice-player1").show();
+            $("#display-text").text("Start Game");
+        });
+    }
+    
+})
